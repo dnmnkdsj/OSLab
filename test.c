da24c82fbcb2b47e5ee16655a98124f1fd648da1
XinHuang98 <763479239@qq.com>
2018-05-10 21:10:22 +0800

    20180510

--- test.c
+++ test.c
@@ -10,39 +10,39 @@ int main() {
 	Elf64_Addr p_header_vaddr;
 	Elf64_Addr p_header_size;
 
-	/* 打开目标文件，读取ELF头部，得到最初的入口地址*/
-	Elf64_Ehdr *e_header = (Elf64_Ehdr*)malloc(sizeof(Elf64_Ehdr)); //使用Elf64_Ehdr类型的结构体eheader来存储文件头
-	FILE *fp = fopen("../hello_world","rb+"); //已只读的方式r打开目标文件
-	fread(e_header, sizeof(Elf64_Ehdr), 1, fp); //从文件中读取一块大小为sizeof(Elf64_Phdr)的内容到pheader中
+
+	Elf64_Ehdr *e_header = (Elf64_Ehdr*)malloc(sizeof(Elf64_Ehdr));
+	FILE *fp = fopen("../hello_world","r+");
+	fread(e_header, sizeof(Elf64_Ehdr), 1, fp);
 	Elf64_Addr origin = e_header->e_entry; //确定入口地址
 
 	/*读取ELF头部后，读取程序头部*/
+
     int p_num;
 	Elf64_Phdr *p_header = (Elf64_Phdr*)malloc(sizeof(Elf64_Phdr));
-	for (int i = 0; i < (int) e_header->e_phnum; i++) //e_phnum Program header table entrycount
+	for (int i = 0; i < (int) e_header->e_phnum; i++)
 	{
 		fread(p_header, sizeof(Elf64_Phdr), 1, fp);
 		/*如果入口地址在段的内存地址中*/
 		if ((p_header->p_paddr < origin) &&
-			((p_header->p_paddr + p_header->p_filesz) > origin))// p_paddr段在内存中的物理地址 p_filesz该段的结区的总字长
+			((p_header->p_paddr + p_header->p_filesz) > origin))
 		{
-			p_header_vaddr = p_header->p_vaddr; //找到程序段,保存段的虚拟地址
-			p_header_size = p_header->p_filesz; //保存段的大小
             p_num = i;
+			p_header_vaddr = p_header->p_vaddr;
+			p_header_size = p_header->p_filesz;
+           break;
 
 		}
 	}
 
-    fseek(fp,(int)(e_header->e_shoff) - sizeof(Elf64_Ehdr) - (int)e_header->e_phnum * sizeof(Elf64_Phdr), SEEK_CUR);//指针跳过程序头部和节区定位到头部
+    fseek(fp,(int)(e_header->e_shoff), SEEK_SET);
 
     Elf64_Shdr *s_header = (Elf64_Shdr*)malloc(sizeof(Elf64_Shdr));
-    Elf64_Shdr *after_sh = (Elf64_Shdr*)malloc(sizeof(Elf64_Shdr));
     Elf64_Off entry_section_offset;
     Elf64_Xword entry_section_size;
     Elf64_Addr new_entry;
 
     int section_num;
-
     for (int i = 0; i < (int)e_header->e_shnum; i++) {
 		fread(s_header, sizeof(Elf64_Shdr), 1, fp);
 		if (s_header->sh_addr+s_header->sh_size == (p_header_vaddr + p_header_size)) {
@@ -54,92 +54,106 @@ int main() {
 		}
 	}
 
-    for (int j = section_num + 1;j<(int)e_header->e_shnum;j++){
-        fread(after_sh, sizeof(Elf64_Shdr),1,fp);
-        after_sh->sh_offset = after_sh->sh_offset + 4096;
-    }
-
     p_header->p_filesz = p_header->p_filesz + 4096;
-	p_header->p_memsz = p_header->p_memsz + 4096; //增加段的大小
-    s_header->sh_size = entry_section_size + 4096; //增加节区的大小
+	p_header->p_memsz = p_header->p_memsz+4096;
+    s_header->sh_size = entry_section_size + 4096;
 	e_header->e_entry = new_entry;
 	e_header->e_shoff = e_header->e_shoff+4096;
 
+
 	char nop[] = {0x90};
 	char helloworld[] = {
-	                      0xbd,0x76,0x53,0x19,0x04,
-	                      0x48,
-                          0x65,0x6c,
-                          0x6c,
-                          0x20,0x57,0x6f,
-                          0x72,0x6c,
-                          0x64,0x21,0x0a,
-                          0x48,0xc7,0xc0,0x01,0x00,0x00,0x00,
-                          0x48,0xc7,0xc7,0x01,0x00,0x00,0x00,
-                          0x48,0xc7,0xc6,0x78,0x00,0x40,0x00,
-                          0x48,0xc7,0xc2,0x0d,0x00,0x00,0x00,
-                          0xbd,0xe9};
+	                     //0x48,0x89,0xe5,
+	                      //0x48,0xc7,0xc0,0x05,0x00,0x00,0x00,
+                          //0x48,0xc7,0xc3,0x01,0x00,0x00,0x00,
+                          //0x48,0xc7,0xc1,0x00,0x00,0x00,0x00,
+                          //0x48,0xc7,0xc2,0xb6,0x01,0x00,0x00,
+                          //0xcd,0x80,
+                         // 0x48,0xc7,0xc0,0x04,0x00,0x00,0x00,
+                         // 0x48,0xc7,0xc1,0x45,0x07,0x40,0x00,
+                         // 0x48,0xc7,0xc2,0x0d,0x00,0x00,0x00,
+                         // 0xcd,0x80,
+                         // 0x48,0xc7,0xc0,0x06,0x00,0x00,0x00,
+                         // 0xcd,0x80,
+                          0x48,0xc7,0xc3,0x30,0x04,0x40,0x00,
+                          0xff,0xe3,
+                          //0x48,0x65,0x6c,0x6c,0x6f,0x20,0x57,
+                          //0x6f,0x72,0x6c,0x64,0x21,0x0a
+			};
 
     fclose(fp);
 
-	int c;
+	int c,num,num2;
 	FILE *source,*write,*keep;
-	source = fopen("../hello_world", "r");
-	write = fopen("../rewrite","w+");//打开文件。
-    keep = fopen("../keep","w+");
+	source = fopen("../hello_world","r+");
+	write = fopen("../rewrite","r+");//打开文件。
+    keep = fopen("../keep","r+");
 
+    fseek(source,entry_section_offset+entry_section_size,SEEK_SET);//从偏移位置开始读取
+    while((num = fgetc(source))!=EOF)
+    {
+        fputc(num,keep);
+    }
+
+    fseek(source,0,SEEK_SET);
     while((c = fgetc(source))!=EOF)//循环读文件到文件尾。
     {
         fputc(c, write);//写目标文件
     }
 
-    fseek(source,entry_section_offset+entry_section_size,SEEK_SET);//从偏移位置开始读取
-    while((c = fgetc(source))!=EOF)
-    {
-        fputc(c,keep);
-    }
-    fclose(source);
+    fseek(write,0,SEEK_SET);//重写e_header
+    fwrite(e_header, sizeof(Elf64_Ehdr),1,write);
 
-    if(-1 == fseek(write,entry_section_offset+entry_section_size,SEEK_SET))
+    fseek(write,e_header->e_phoff+p_num* sizeof(Elf64_Phdr),SEEK_SET);//重写p_header
+    fwrite(p_header, sizeof(Elf64_Phdr),1,write);
+
+    Elf64_Phdr *after_p_header = (Elf64_Phdr*)malloc(sizeof(Elf64_Phdr));//重写p_header之后的节区偏移量
+    fseek(source,e_header->e_phoff+ sizeof(Elf64_Phdr)*(p_num+1),SEEK_SET);
+    for(int n = 0; n <(int)e_header->e_phnum-p_num-1;n++)
     {
-        printf("fseek error");
+        if(n!=4&&n!=2&&n!=3)
+        {
+            fread(after_p_header, sizeof(Elf64_Phdr),1,source);
+            after_p_header->p_offset = after_p_header->p_offset+4096;
+            fwrite(after_p_header, sizeof(Elf64_Phdr),1,write);
+        } else{
+            fread(after_p_header, sizeof(Elf64_Phdr),1,source);
+            after_p_header->p_offset = after_p_header->p_offset;
+            fwrite(after_p_header, sizeof(Elf64_Phdr),1,write);
+        }
+
     }
 
-    if(1 != fwrite(helloworld,sizeof(helloworld),1,write))
-    {
-        printf("fwrite error 1");
-    }
-
-	for(int i; i <4096 - sizeof(helloworld);i++)
+    fseek(write,entry_section_offset+entry_section_size,SEEK_SET);//重写section
+    fwrite(helloworld, sizeof(helloworld),1,write);
+    for(int i=0; i <4096 - sizeof(helloworld);i++)
     {
         fwrite(nop, sizeof(nop),1,write);
     }
 
     fseek(keep,0,SEEK_SET);
-
-    while((c=fgetc(keep))!=EOF)
+    while((num2 = fgetc(keep))!=EOF)//循环读文件到文件尾。
     {
-        fputc(c,write);
+        fputc(num2, write);//写目标文件
     }
-    fflush(write);
-    fseek(write,0,SEEK_SET);
-    fwrite(e_header, sizeof(e_header),1,write);
-    fseek(write,(p_num-1)*sizeof(Elf64_Phdr),SEEK_CUR);
-    fwrite(p_header, sizeof(p_header),1,write);
-    fseek(write,(int)(e_header->e_shoff) - sizeof(Elf64_Ehdr) - (int)e_header->e_phnum * sizeof(Elf64_Phdr),SEEK_CUR);
-    fwrite(s_header, sizeof(s_header),1,write);
 
+    fseek(write,e_header->e_shoff+section_num*sizeof(Elf64_Shdr),SEEK_SET);
+    fwrite(s_header, sizeof(Elf64_Shdr),1,write);
+
+    Elf64_Shdr *after_s_header = (Elf64_Shdr*)malloc(sizeof(Elf64_Shdr));//重写p_header之后的节区偏移量
+    fseek(source,(int)e_header->e_shoff-4096 + (section_num+1)*sizeof(Elf64_Shdr),SEEK_SET);
+    for(int m = 0; m <(int)e_header->e_shnum-section_num-1;m++)
+    {
+        fread(after_s_header, sizeof(Elf64_Shdr),1,source);
+        after_s_header->sh_offset = after_s_header->sh_offset+4096;
+        fwrite(after_s_header, sizeof(Elf64_Shdr),1,write);
+    }
+
+    fclose(source);
     fclose(write);
     fclose(keep);
 
-    //调试用
-    Elf64_Ehdr *test_e_header = (Elf64_Ehdr*)malloc(sizeof(Elf64_Ehdr)); //使用Elf64_Ehdr类型的结构体eheader来存储文件头it 
-    FILE *test = fopen("../testheader","r+"); //已只读的方式r打开目标文件
-    fwrite(e_header, sizeof(e_header),1,test);
-    fflush(test);
-    fseek(test,0,SEEK_SET);
-    fread(test_e_header, sizeof(Elf64_Ehdr), 1, test); //从文件中读取一块大小为sizeof(Elf64_Phdr)的内容到pheader中
-    fclose(test);
+
 }
 
 
